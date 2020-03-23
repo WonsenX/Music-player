11249168e0d5a89462cc9e7af788a4f55276413b
WonsenX <vantanthong2012@gmail.com>
2020-03-23 17:18:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ $(function()
         artist: 'Marr D Bin',
 		name: 'Di Theo Anh',
 		url: 'Musics/Di Theo Anh.mp3',
-        picture: 'https://raw.githubusercontent.com/WonsenX/Music-player/master/Background/406924e492ad7d7fc17bc3479727f82b.jpg'
+        picture: 'https://raw.githubusercontent.com/WonsenX/Music-player/master/Image/_1.jpg'
     },
     {
         artist: 'Ngoc Doli',
